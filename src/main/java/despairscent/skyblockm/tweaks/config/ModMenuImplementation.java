ef5f7b8500fbabcd85e83bf93907b394cca1cf71
Ilya Dudelzak <DIA070611@gmail.com>
2026-09-12 15:53:26 +0300

    Make Cloth Config and Mod Menu strictly optional with fallback NoticeScreen

--- src/main/java/despairscent/skyblockm/tweaks/config/ModMenuImplementation.java
+++ src/main/java/despairscent/skyblockm/tweaks/config/ModMenuImplementation.java
@@ -2,15 +2,33 @@ package despairscent.skyblockm.tweaks.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import despairscent.skyblockm.tweaks.ModUtils;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.screen.NoticeScreen;
+
+import static despairscent.skyblockm.tweaks.ModUtils.i18n;
 
 public class ModMenuImplementation implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        if (FabricLoader.getInstance().isModLoaded("cloth-config2")) {
-            return ClothConfigImplementation::generate;
+        if (FabricLoader.getInstance().isModLoaded("cloth-config") || FabricLoader.getInstance().isModLoaded("cloth-config2")) {
+            return ClothConfigHelper.getScreenFactory();
         } else {
-            return parent -> null;
+            return parent -> new NoticeScreen(
+                    () -> {
+                        if (ModUtils.CLIENT != null) {
+                            ModUtils.CLIENT.setScreen(parent);
+                        }
+                    },
+                    i18n("config.missing_cloth.title"),
+                    i18n("config.missing_cloth.desc")
+            );
+        }
+    }
+
+    private static class ClothConfigHelper {
+        private static ConfigScreenFactory<?> getScreenFactory() {
+            return ClothConfigImplementation::generate;
         }
     }
 }
