711a15207c815e7e0669bff7392069dfe59346be
Ilya Dudelzak <DIA070611@gmail.com>
2026-09-12 15:33:52 +0300

    Fix NullPointerException during client initialization due to early setScreen call

--- src/main/java/despairscent/skyblockm/tweaks/ModLoader.java
+++ src/main/java/despairscent/skyblockm/tweaks/ModLoader.java
@@ -34,6 +34,7 @@ public class ModLoader implements ClientModInitializer {
         );
 
         net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            despairscent.skyblockm.tweaks.ModUtils.CLIENT = client;
             SkyBlockPackManager.onClientStarted(client);
         });
     }
